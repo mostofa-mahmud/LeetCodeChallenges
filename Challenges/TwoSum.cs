0f01fa0551e21d69d01f15ceac09f57e78bb133e
mostofa-mahmud <mostofamahmud23@gmail.com>
2024-05-27 21:30:44 +0600

    Add project files.

---
+++ Challenges/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges.Challenges
+{
+    public class Calculate
+    {
+        /*public static void Print() {
+
+            Console.WriteLine("heloo");
+        }*/
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < (nums.Length - 1); i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeChallenges.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<int> numbers = new List<int> {2, 7, 11, 15};
+            int[] numbers = { 2, 7, 11, 15};
+            int[] abc = Calculate.TwoSum(numbers,9);
+            Console.WriteLine(abc);
+            Console.ReadLine();
+           
+        }
+    }
+}
